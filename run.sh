d6995b064d3cf32eefa3ab95452f5a63f9e7d326
LWMmiaomiao <guoziying22@mails.ucas.ac.cn>
2024-12-03 11:38:09 +0800

    prj4 Acore

--- run.sh
+++
@@ -1,8 +0,0 @@
-make clean
-make all
-if [ "$1" == "smp" ];then
-	make run-smp
-else
-	make run
-fi
-make cursor
---
+++ test/test_project4/swaptest.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdint.h>
+#include <time.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
+#define TEST_POINT 70
+int total;
+void test(int a, int b, int c){
+	total = a+b+c;
+	if(b!=c){
+		printf("has error");
+	}
+}
+int main(int argc, char *argv[])
+{
+	int error_time = 0;
+	srand(clock());
+	int data[TEST_POINT];
+	for (int i = 0; i < TEST_POINT; ++i) {
+		data[i] = rand() & 1023;
+		// printf("%ld\n", data[i]);
+	}
+	for (long int i = 0; i < 4096 * TEST_POINT; i += 4096) {
+		*(int *)i = data[i / 4096];
+		// printf("0x%lx, %ld, %ld\n", i, data[i / 4096], *(long *)i);
+	}
+	for (long int i = 0; i < 4096 * TEST_POINT; i += 4096) {
+		test(i, data[i / 4096], *(int *)i);
+		printf("0x%lx, %d, %d  ", i, data[i / 4096], *(int *)i);
+		// if (*(int *)i != data[i / 4096]) {
+		// 	printf("Error! ");
+		// 	error_time++;
+		// 	// return 0;
+		// }
+	}
+
+	printf("\nTOTAL %d TIMES, ERROR %d TIMES.\n", TEST_POINT, error_time);
+	if(!error_time)
+		printf("Success!\n");
+	return 0;
+}
